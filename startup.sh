909bb3f256b3b163b51c028aac2f4889a00b882a
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-08-17 16:23:05 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,10 @@
 echo "Starting A (SQL Monitor)..."
 python3 a_sql_monitor.py &
 
-echo "Starting B (Collector Monitor with TCP+HTTP trigger)..."
-python3 b_collector_monitor.py &
+#echo "Starting B (Collector Monitor with TCP+HTTP trigger)..."
+#python3 b_collector_monitor.py &
 
 # Keep the container alive (wait for any process to exit)
 wait -n
+
 
